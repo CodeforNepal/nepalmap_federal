ab38db324262cb6f4b5ab4c448a59355a6a5afe9
Amit Chaudhary <meamitkc@gmail.com>
2019-07-07 23:55:55 +0545

    Integrate religion date to demographic section

--- wazimap_np/demographics.py
+++ wazimap_np/demographics.py
@@ -1,9 +1,10 @@
-from collections import OrderedDict
+from wazimap.data.utils import get_stat_data
 
 
-from wazimap.data.tables import get_datatable, get_model_from_fields
-from wazimap.data.utils import get_stat_data, get_objects_by_geo, \
-    calculate_median
+def get_highest_value(data):
+    data_copy = data.copy()
+    data_copy.pop('metadata')
+    return sorted(data_copy.items(), key=lambda x: x[1]['values']['this'], reverse=True)[0][1]
 
 
 def get_demographics_profile(geo_code, geo_level, session):
@@ -20,4 +21,11 @@ def get_demographics_profile(geo_code, geo_level, session):
             'values': {'this': total_pop}
         }
     }
+
+    religion_data, _ = get_stat_data(['religion_name'], geo_level,
+                                  geo_code, session)
+    most_populous_religion = get_highest_value(religion_data)
+
+    demographic_data['religion_distribution'] = religion_data
+    demographic_data['most_populous_religion'] = most_populous_religion
     return demographic_data
--- wazimap_np/tables.py
+++ wazimap_np/tables.py
@@ -1,8 +1,17 @@
 from wazimap.data.tables import FieldTable, SimpleTable
 
+
+# Population tables
 FieldTable(['sex'],
            id='population',
            universe='Population',
            description='Total Population',
+           dataset='National Population and Housing Census 2011',
+           year='2011')
+
+FieldTable(['religion_name'],
+           id='religion',
+           universe='Population',
+           description='Population by Religion',
            dataset='National Population and Housing Census 2011',
            year='2011')
